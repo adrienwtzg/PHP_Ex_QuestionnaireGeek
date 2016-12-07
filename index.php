31fd7362446882363389c53e325095c7a061d9f3
adrienwtzg <adrien.wtzg@eduge.ch>
2016-12-07 08:59:21 +0100

    Ajout fichiers débuts

---
+++ index.php
@@ -0,0 +1,172 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Test Geek</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="testgeek.css" rel="stylesheet" type="text/css" />    
+	</head>
+	<body>
+		<div id="titre">
+			<table class="tableau1">
+				<tr>
+				  <td>
+					<h1>Est-tu un vrai geek?</h1></td>
+				</tr>
+			</table>
+		</div>
+		<div id="contenu">			
+			<span class="gras">
+				Salut, pour savoir si tu es vraiment un geek répond à ce questionnaire puis clique sur "Destin"
+				<br/>
+				(note : les réponses données par ce questionnaire sont 100% fiables et garanties à vie +2 jours)
+				<br/>
+			</span>
+			<form method="post" action="repondeur.php">
+				<table class="tableau2">
+                    <tr>
+                        <td>
+                            1. Que fais tu lorsque tu te lève le matin?
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+							<input type="radio" name="R1" value="1">Tu fais un gros calin à ton doudou pandi-panda</input><br/> 
+							<input type="radio" name="R1" value="2">Tu jete un oeil à ton bot wow pour savoir s'il a bien farmer toute la nuit</input><br/>
+							<input type="radio" name="R1" value="0">Tu te lève et mange tes céréales favoris: Smacks de Kellogs</input><br/>
+			 				<input type="radio" name="R1" value="3">Tu tape un bon coup sur la tête de Dark Vador qui te sert de réveil</input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            2. Lorsque tu regarde les filles/garçons dans la rue, tu regarde d'abord :
+                        </td>						
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R2" value="0">Si elle/il a de beaux yeux</input><br/>
+							<input type="radio" name="R2" value="2">Si elle/il a un animal de companie</input><br/>
+			 			 	<input type="radio" name="R2" value="3">Si elle/il possède le dernier téléphone portable du marché</input><br/>
+							<input type="radio" name="R2" value="1">Si elle/il possède des lunettes de soleil "Rebane"</input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            3. Tu aimerais bien sortir avec (attention il y a un piège) :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R3" value="0">Un blond / une blonde</input><br/>
+			 				<input type="radio" name="R3" value="1">Un camionneur / une camionneuse</input><br/>
+							<input type="radio" name="R3" value="3">Nyan Cat / Nyane Cat</input><br/>
+			 				<input type="radio" name="R3" value="2">Un gobelin / une gobeline</input>						
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            4. Le film qui te motive le plus c'est :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R4" value="2">Une journée en enfer</input><br/>
+			 				<input type="radio" name="R4" value="0">American Pie</input><br/>
+			 				<input type="radio" name="R4" value="3">Dune</input><br/>
+							<input type="radio" name="R4" value="1">Twilight: Fascination</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            5. Si tu devais donner un nouveau nom à ton père, tu l'appellerais :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R5" value="1">Duke Nukem</input><br/>
+			 				<input type="radio" name="R5" value="3">James T. Kirk</input><br/>
+			 				<input type="radio" name="R5" value="0">Paris Hilton</input><br/>
+							<input type="radio" name="R5" value="2">Alf</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            6. Lorsque tu vois tes amis tu adore :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R6" value="0">Jouer au docteur</input><br/>
+			 				<input type="radio" name="R6" value="1">Regarder les teletubies</input><br/>
+			 				<input type="radio" name="R6" value="2">Créer des pages Web HTML Strict 1.1</input><br/>
+							<input type="radio" name="R6" value="3">Jouer au cartes "Magic: l'assemblée"</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            7. Tu collectionne ou collectionnerais bien :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R7" value="3">Des boules à neige</input><br/>
+			 				<input type="radio" name="R7" value="1">Des timbres</input><br/>
+			 				<input type="radio" name="R7" value="2">Des échecs amoureux</input><br/>
+							<input type="radio" name="R7" value="0">Des savons pour le bain</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            8. Lorsque tu veux aborder un garçon/une fille tu lui dis :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R8" value="0">Salut! J'ai complétement envie de planter ma graine avec toi</input><br/>
+			 				<input type="radio" name="R8" value="3">Allo, ich bin Rodolphe! Ca te dis un ubuntu cola avec moi?</input><br/>
+			 				<input type="radio" name="R8" value="2">Heu bon-bonjour! Je peux t'offrir un ve-verre d'eau au toilette s'il te plait?</input><br/>
+							<input type="radio" name="R8" value="1">Hi coquin/coquine ca farte? C'est quoi ton p'tit nom propre?</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            9. Ton hobbie favoris serait :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R9" value="3">Jouer au Rubik's Cube</input><br/>
+			 				<input type="radio" name="R9" value="0">Le sport matelas</input><br/>
+			 				<input type="radio" name="R9" value="1">Faire du rugby</input><br/>
+							<input type="radio" name="R9" value="2">Les jeux en ligne</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            10. Tu crois :
+                        </td>
+					</tr>						
+					<tr>
+                        <td>
+                            <input type="radio" name="R10" value="3">A la force</input><br/>
+			 				<input type="radio" name="R10" value="2">Que J.R.R Tolkien n'est pas mort</input><br/>
+			 				<input type="radio" name="R10" value="0">Que "Linux" est le nom d'un pinguin</input><br/>
+							<input type="radio" name="R10" value="1">En toi</input>
+                        </td>
+                    </tr>
+					<tr>
+                        <td>
+                            Voilà c'est fini! appuie sur "Destin" pour tout savoir... hehe :-)
+                        </td>
+					</tr>
+					<tr>
+                        <td id="derniereCase">						 
+							<input type="submit" value="Destin" name="B1" class="mesBoutons"/>
+			 				&nbsp;
+							<input type="reset" value="Tout effacer" name="B2" class="mesBoutons"/>
+						</td>
+					</tr>
+				</table>			
+			</form>
+		</div>
+	</body>
+</html>
---
+++ repondeur.php
@@ -0,0 +1,50 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>inscription.html</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="testgeek.css" rel="stylesheet" type="text/css" />    		
+	</head>
+	<body>
+		<div id="reponse">
+			<p>
+		<?php  	
+			// On declare une constante pour le nombre de question
+		
+			// La fonction verification() permet de savoir si l'utilisateur à bien répondu a toutes les questions, elle renvoi true ou false
+			
+			
+			
+			// La fonction points() permet de calculer le nombre de points obtenus, elle renvoi un nombre
+			
+			
+			
+			// On appel la fonction verification() et si c'est ok on calcul les points avec la fonction points()
+			
+				// Si le score est de 30, on est un vrai geek
+				echo "T'es un(e) vrai geek!!! Même le capitaine Kirk t'envie à mort... tu es plus fort que Chuck Norris (ou pas) mais c'est dommage que tu ne sois attiré(e) que par Nyan Cat!";
+
+					// Sinon si le score est entre 20 et 29, on est pas un vrai geek mais un gamer ou un noob
+					echo "Tu n'es pas un vrai geek, mais plutôt un gamer ou un noob ;-) Quoiqu'il en soit il faut encore que tu travail tes réponses avant de prétendre être un geek!";
+					
+						// Sinon si le score est entre 10 et 19, on est très loin d'être un vrai geek
+						echo "Tu es loin d'être un vrai geek! Là j'imagine que tu te dis 'ouf sauvé!', mais en réalité je suis sur que les gobelins te manque inconsciemment...";
+						
+							// Sinon si le score est entre 1 et 9, on est a des années lumière d'être un vrai geek
+							echo "Tu es a des années lumière d'un vrai geek! A mon avis tu es quelqu'un de bien malgré ton esprit mal tourné ;-) Allez recommence et fait un effort!";
+							
+								// Sinon le score est de 0, on est pas un geek mais un type qui ne connait rien à rien
+								echo "A défaut d'être un geek tu es équivalent à la valeur null ou nil... Malgré tout tu pourrais peut-être te recycler dans le commerce de savons?";
+							
+							
+			// Sinon c'est que c'est pas OK, il faut afficher un message à l'utilisateur pour lui dire de bien tout remplir
+			echo "Tu n'as pas rempli toute les cases!";	
+			
+		?>		
+			</p>
+			<form method="post" action="testgeek.html">
+				<input id="boutonRetour" type="submit" value="Retour" class="mesBoutons"/>
+			</form>
+		</div>
+	</body>
+</html>
